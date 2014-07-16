67dda180e94722dbb50cfaa8bd385a264a4578b1
Marc Blümner <m.bluemner@liquidlabs.de>
2014-07-16 18:16:38 +0200

    added XSRF-TOKEN to applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_filter :set_csf_cookie_for_ng
+
+  private
+
+  def set_csf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_autenticity_token if protect_against_forgery?
+  end
+
+  def verified_request?
+    super || form_autenticity_token == request.headers['HTTP_X_XSRF_TOKEN']
+  end
 end
