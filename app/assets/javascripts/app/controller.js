55b8aa2e7ee96075c4ab614aae53369fbd3ea264
Marc Blümner <m.bluemner@liquidlabs.de>
2014-07-17 12:26:26 +0200

    injected sessionservice

--- app/assets/javascripts/app/controller.js
+++ app/assets/javascripts/app/controller.js
@@ -1,6 +1,7 @@
 angular.module('myApp.controllers', [])
 .controller('HomeController', 
-           function($scope, ArticleService) {
+           function($scope, session, SessionService,  ArticleService, Share) {
+             $scope.user = session.user;
             ArticleService.getLatestFeed()
             .then(function(data) {
               $scope.articles = data;
