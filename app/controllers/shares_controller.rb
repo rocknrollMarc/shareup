b00668bc48374a281172194da91a52d8c43f44ec
Marc Blümner <m.bluemner@liquidlabs.de>
2014-07-17 13:15:49 +0200

    respond to json requests!

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,2 +1,19 @@
 class SharesController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create]
+  respond_to :json
+
+  def index 
+    render json: []
+  end
+
+  def create
+    link = params[:url]
+
+    share = Share.create(share_params)
+    render  status: 200,
+      json: {
+        success: share.persisted?,
+        share_id: share.id
+    }
+  end
 end
